5e58e9e794840b01d39f93decdaf9b9a7a95ad95
seanBE <SeanBE@users.noreply.github.com>
2022-12-24 16:50:56 +0000

    set up http-proxy via vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
+import dns from 'node:dns';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://github.com/vitejs/vite/discussions/7620#discussioncomment-3166670
+// https://github.com/nodejs/node/issues/40537#issuecomment-1237194449
+// node no longer resorts IP address lookups and takes OS preference
+dns.setDefaultResultOrder('ipv4first');
+
 export default defineConfig({
   server: {
    proxy: {
-      '/api': {
-        target: "http://localhost:8080/",
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+		  '/api': "http://localhost:8080",
     }
   },
   plugins: [react()],
